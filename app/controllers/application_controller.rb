8456462fc3f19af7632ad1bbb12fd444375066b8
shadowfox3735 <normanpan180@gmail.com>
2019-09-03 13:34:50 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,14 +3,14 @@ class ApplicationController < Sinatra::Base
     set :public_folder, 'public'
     set :views, 'app/views'
   end
+
+  get '/recipes/new' do
+    erb :new
+  end
   
   get '/recipes' do
     @recipes = Recipee.all
     erb :index
-  end
-
-  get '/recipes/new' do
-    erb :new
   end
   
   post '/recipes' do
